e6374d716c4da5544db009d6026d695c26109488
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-04-18 22:28:23 -0400

    updated get function

--- server.js
+++ server.js
@@ -112,8 +112,12 @@ app.get('/animals', (req, res) => {
     res.sendFile(path.join(__dirname, './public/animals.html'));
 });
 
-app.get('./zookeepers', (req, res) => {
+app.get('/zookeepers', (req, res) => {
     res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
